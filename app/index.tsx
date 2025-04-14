56c4b9ed80a09b302fb979f0216f008288ca8220
RUDRANSHFLY <mayamayavir@gmail.com>
2025-04-14 16:05:12 +0530

    custom colors add

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-5xl font-bold">Taryn Jagoda.</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl font-bold text-primary">Taryn Jagoda.</Text>
     </View>
   );
 }
