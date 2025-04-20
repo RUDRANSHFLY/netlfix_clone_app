7c9c5a70bca5d84e5f866fe0ee476ee265c29eda
RUDRANSHFLY <mayamayavir@gmail.com>
2025-04-20 17:06:31 +0530

    movie detail page done

--- interface/interfaces.d.ts
+++ interface/interfaces.d.ts
@@ -15,37 +15,45 @@ interface Movie {
     vote_count: number;
 }
 
-interface TrendingMovie{
-    searchTerm : string;
-    movie_id : number;
-    title : string;
-    count : number;
-    poster_url : string;
+interface TrendingMovie {
+    searchTerm: string;
+    movie_id: number;
+    title: string;
+    count: number;
+    poster_url: string;
 }
 
 interface MovieDetails {
-    adult : boolean;
-    backdrop_path : string | null ;
-    belongs_to_collection : {
-        id : number ;
-        name : string;
-        poster_path : string;
-        backdrop_path : string
-    } | null ;
-    budget : number ;
-    genres : {
-        id : number;
+    adult: boolean;
+    overview: string;
+    backdrop_path: string | null;
+    poster_path: string | null;
+    belongs_to_collection: {
+        id: number;
+        name: string;
+        poster_path: string;
+        backdrop_path: string
+    } | null;
+    budget: number | undefined;
+    genres: {
+        id: number;
+        name: string;
+    }[];
+    production_companies:{
         name : string;
     }[];
-    status : string;
-    tagline : string;
-    title : string;
-    video : boolean;
-    vote_average : number;
-    vote_count : number;
+    status: string;
+    tagline: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+    release_date: string;
+    runtime : number;
+    revenue : number;
 }
 
-interface TrendingCardPRops{
-    movie : TrendingMovie;
-    index : number;
+interface TrendingCardPRops {
+    movie: TrendingMovie;
+    index: number;
 }
