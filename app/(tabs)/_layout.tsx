81530b1f3d76c9d31a1f25478b8b0e2db53e4ff4
RUDRANSHFLY <mayamayavir@gmail.com>
2025-04-20 12:49:32 +0530

    trending movie card done

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,88 +1,93 @@
 import { Tabs } from "expo-router";
 import React from "react";
 
-
 //? Icons
 
 import { icons } from "@/constants/icons";
 import TabIcon from "@/components/tab-icon";
+import { StatusBar } from "react-native";
 
 const Layout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarShowLabel : false,
-        tabBarItemStyle : {
-          width : '100%',
-          height : '100%',
-          justifyContent : "center",
-          alignItems : "center",
-        },
-        tabBarStyle : {
-          backgroundColor  : '#0f0D23',
-          borderRadius : 50,
-          marginHorizontal : 20,
-          marginBottom : 36,
-          height : 52,
-          position : "absolute",
-          overflow : "hidden",
-          borderWidth : 1,
-          borderColor : '#ofod23'
-        }
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <>
-              <TabIcon focused={focused} title="Home" icon={icons.home} />
-            </>
-          ),
+    <>
+      <StatusBar hidden={true} />
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarItemStyle: {
+            width: "100%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          },
+          tabBarStyle: {
+            backgroundColor: "#0f0D23",
+            borderRadius: 50,
+            marginHorizontal: 20,
+            marginBottom: 36,
+            height: 52,
+            position: "absolute",
+            overflow: "hidden",
+            borderWidth: 1,
+            borderColor: "#ofod23",
+          },
         }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          title: "Search",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <>
-              <TabIcon focused={focused} title="Search" icon={icons.search} />
-            </>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="saved"
-        options={{
-          title: "Saved",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <>
-              <TabIcon focused={focused} title="Saved" icon={icons.save} />
-            </>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <>
-              <TabIcon focused={focused} title="Profile" icon={icons.person} />
-            </>
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <>
+                <TabIcon focused={focused} title="Home" icon={icons.home} />
+              </>
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          options={{
+            title: "Search",
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <>
+                <TabIcon focused={focused} title="Search" icon={icons.search} />
+              </>
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="saved"
+          options={{
+            title: "Saved",
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <>
+                <TabIcon focused={focused} title="Saved" icon={icons.save} />
+              </>
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <>
+                <TabIcon
+                  focused={focused}
+                  title="Profile"
+                  icon={icons.person}
+                />
+              </>
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 };
-
-
 
 export default Layout;
--- components/trending-card.tsx
+++ components/trending-card.tsx
@@ -1,17 +1,27 @@
 import { Link } from 'expo-router';
 import React from 'react';
-import { Image, TouchableOpacity, View } from 'react-native';
+import { Image, TouchableOpacity, View , Text } from 'react-native';
+import MaskedView from '@react-native-masked-view/masked-view';
+import { images } from '@/constants/images';
 
 
 
-const TrendingCard = ({ movie : {movie_id , title , poster_url } , index} : TrendingCardProps) => {
+
+const TrendingCard = ({ movie : {movie_id , title , poster_url } , index} : TrendingCardPRops ) => {
     return (
        <Link href={`/movies/${movie_id}`} asChild>
         <TouchableOpacity className='w-32 relative pl-5'>
             <Image source={{uri : poster_url}} className='w-32 h-48 rounded-lg' resizeMode='cover' />
             <View className='absolute bottom-9 -lef-3.5 px-2 py-1 rounded-full'>
-                
+                <MaskedView maskElement={<Text className="font-bold text-white text-6xl">
+                    {index+1}
+                </Text>} >
+                <Image source={images.rankingGradient} className='size-14' resizeMode='cover' />
+                 </MaskedView>
             </View>
+            <Text className='text-sm text-light-200 font-bold mt-2' numberOfLines={1}>
+                    {title}
+            </Text>
         </TouchableOpacity>
        </Link>
     );
